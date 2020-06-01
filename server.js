44cf24426d8ce710287d1d4a06966a4dcdfc05f5
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-06-01 16:02:40 +0100

    Test out ecs build mounts

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const requestListener = function (req, res) {
   console.log('Hitting: 🔗', req.url);
+  const [_, path] = req.url.split('/pathcheck?path=');
+  if (path) {
+    fs.access(path, fs.constants.OK | fs.constants.R_OK, err => {
+      console.log(`Was ${path} found and readable..: ${err ? 'No, with error '.concat(err.message) : 'Yes'} `);
+    });
+  }
   res.writeHead(200);
   res.end('Hello, from the other side!!! Again');
 }
